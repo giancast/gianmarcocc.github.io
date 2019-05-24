2a97a45b574a111c14840b31257bb802f363962f
Becario STIC2 <BecarioSTIC2@cementoslima.net.pe>
2019-05-24 12:01:30 -0500

    primer commit

---
+++ precache-manifest.61b3441d060308ff2eeb48ca745d1645.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d841b7d06080eacf33f95bb539491b1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a3981b21039c30957bf1",
+    "url": "/static/css/main.0010486f.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "a3981b21039c30957bf1",
+    "url": "/static/js/main.a7d9c19d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
